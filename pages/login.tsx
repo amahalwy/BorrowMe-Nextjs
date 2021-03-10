155ad9105b9e091f78e1f123d60c33e73a5f37e9
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-10 16:15:36 -0500

    Remove testing on ts

--- pages/login.tsx
+++ pages/login.tsx
@@ -12,28 +12,11 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-// interface PersonProps {
-//   name: string;
-//   age: number;
-//   height: number;
-// }
-
-const Person = ({ name }) => {
-  return (
-    <Box>
-      <p>Name: </p>
-      {/* <p>Age: {age}</p>
-      <p>Height: {height ? height : "Not given"}</p> */}
-    </Box>
-  );
-};
-
 export default function Login() {
   return (
     <Box>
       This is the login page!
       <Button bg="brand.200">Open Modal</Button>
-      <Person name={user} />
     </Box>
   );
 }

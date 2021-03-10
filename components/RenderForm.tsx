e65d4a2963295cd7c9e52fe7dcde6ba3fec156e1
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-10 15:52:16 -0500

    Fix renderform's return value from Element[] to jsx component

--- components/RenderForm.tsx
+++ components/RenderForm.tsx
@@ -4,23 +4,27 @@ import InputFields from "./InputFields";
 import SignupInputs from "../generals/objects/SignupInputs";
 
 const RenderForm = () => {
-  return SignupInputs.map((element, i) => {
-    if (element.length === 1) {
-      return (
-        <Box m="4px 0" key={i}>
-          <InputFields inputField={element[0]} />
-        </Box>
-      );
-    } else {
-      return (
-        <Box d="flex" key={i}>
-          {element.map((input, i) => {
-            return <InputFields inputField={input} key={i} />;
-          })}
-        </Box>
-      );
-    }
-  });
+  return (
+    <Box>
+      {SignupInputs.map((element, i) => {
+        if (element.length === 1) {
+          return (
+            <Box m="4px 0" key={i}>
+              <InputFields inputField={element[0]} />
+            </Box>
+          );
+        } else {
+          return (
+            <Box d="flex" key={i}>
+              {element.map((input, i) => {
+                return <InputFields inputField={input} key={i} />;
+              })}
+            </Box>
+          );
+        }
+      })}
+    </Box>
+  );
 };
 
 export default RenderForm;

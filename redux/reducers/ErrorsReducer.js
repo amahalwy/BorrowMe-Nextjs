2b2597c7dd35a77f21d9f7030ab8a77dd5c69ae5
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-14 13:53:17 -0400

    Trial fix for errorsReducer

--- redux/reducers/ErrorsReducer.js
+++ redux/reducers/ErrorsReducer.js
@@ -5,10 +5,12 @@ import postingsErrorsReducer from "./postingsErrorsReducer";
 import requestsErrorsReducer from "./postingsErrorsReducer";
 import bookingsErrorsReducer from "./postingsErrorsReducer";
 
-export default combineReducers({
+const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   user: usersErrorsReducer,
   postings: postingsErrorsReducer,
   requests: requestsErrorsReducer,
   bookings: bookingsErrorsReducer,
 });
+
+export default errorsReducer;

98adfa871e65442de5fc861a59771a8302dd63c5
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-17 16:57:58 -0400

    Re-styled and hooked up redux with navbar to keep track of current user

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,75 +1,125 @@
-import {
-  background,
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  Button,
-  Heading,
-  Link,
-} from "@chakra-ui/react";
-
+import { Box, Button, Heading, Image } from "@chakra-ui/react";
 import React from "react";
 import { useRouter } from "next/router";
+import { connect, useDispatch } from "react-redux";
+import { logout } from "../redux/actions/sessionActions";
 
-const NavBar = () => {
+const buttonStyles = {
+  borderRadius: "25px",
+  border: "2px solid white",
+  background: "#a2d3c2",
+  color: "#fff",
+  font: "bold",
+  padding: "5px 15px",
+};
+
+const LoggedIn = () => {
+  const dispatch = useDispatch();
   const router = useRouter();
-  const buttonStyles = {
-    borderRadius: "25px",
-    border: "2px solid white",
-    margin: "0 40px 0 0",
-    // fontWeight: "700",
-    background: "#a2d3c2",
-    color: "#fff",
-    font: "bold",
-    padding: "5px 15px",
-    // _hover: {
-    //   bg: "#779e91",
-    //   color: "#000",
-    //   border: "#000",
-    // },
-  };
-
   return (
-    <Box w="100%">
-      <Box w="40%">
-        <Heading
-          color="#fff"
-          as="h2"
-          position="fixed"
-          fontSize="24px"
-          m="24px 0 0 180px"
-          fontWeight="semibold"
+    <Box w="100%" d="flex" h="66px" bg="#a2d3c2" shadow="xl">
+      <Box w="75%">
+        <Box
+          w="17%"
+          ml="6%"
+          h="100%"
+          d="flex"
+          alignItems="center"
+          justifyContent="space-around"
         >
-          Borrow Me
-        </Heading>
-      </Box>
-
-      <Box
-        bg="#a2d3c2"
-        height="68px"
-        separator=" "
-        justifyContent="flex-end"
-        alignItems="center"
-        display="flex"
-      >
-        <Box w="290px">
-          <Button
-            onClick={() => router.push("/signup")}
-            style={buttonStyles}
-            _hover={{ border: "1px solid red" }}
-            // _hover={buttonStyles._hover}
+          <Box
+            w="25%"
+            h="90%"
+            borderRadius="10px"
+            _hover={{ bg: "rgba(0,0,0,0.4)", cursor: "pointer" }}
           >
-            Signup
-          </Button>
-          <Button onClick={() => router.push("/login")} style={buttonStyles}>
-            Login
-          </Button>
+            <Image
+              w="100%"
+              h="100%"
+              src="https://borrowme-pro.s3.us-east-2.amazonaws.com/icons/bm-logo.png"
+            />
+          </Box>
+          <Box>
+            <Heading color="white" as="h2" fontSize={30} fontWeight="bold">
+              BorrowMe
+            </Heading>
+          </Box>
         </Box>
+      </Box>
+      <Box w="25%" d="flex" alignItems="center" justifyContent="center">
+        <Button
+          onClick={() => {
+            dispatch(logout());
+            router.push("/login");
+          }}
+          style={buttonStyles}
+          _hover={{ border: "1px solid red" }}
+        >
+          Logout
+        </Button>
       </Box>
     </Box>
   );
 };
 
-export default NavBar;
+const LoggedOut = () => {
+  const router = useRouter();
+  return (
+    <Box w="100%" d="flex" h="66px" bg="#a2d3c2" shadow="xl">
+      <Box w="75%">
+        <Box
+          w="17%"
+          ml="6%"
+          h="100%"
+          d="flex"
+          alignItems="center"
+          justifyContent="space-around"
+        >
+          <Box
+            w="25%"
+            h="90%"
+            borderRadius="10px"
+            _hover={{ bg: "rgba(0,0,0,0.4)", cursor: "pointer" }}
+          >
+            <Image
+              w="100%"
+              h="100%"
+              src="https://borrowme-pro.s3.us-east-2.amazonaws.com/icons/bm-logo.png"
+            />
+          </Box>
+          <Box>
+            <Heading color="white" as="h2" fontSize={30} fontWeight="bold">
+              BorrowMe
+            </Heading>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        pr="10%"
+        ml="5%"
+        w="20%"
+        d="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Button onClick={() => router.push("/signup")} style={buttonStyles}>
+          Signup
+        </Button>
+        <Button onClick={() => router.push("/login")} style={buttonStyles}>
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+const NavBar = (props) => {
+  return <>{props.isAuthenticated ? <LoggedIn /> : <LoggedOut />}</>;
+};
+
+const mSTP = (state) => ({
+  isAuthenticated: state.session.isAuthenticated,
+  currentUser: state.session.user,
+});
+
+export default connect(mSTP)(NavBar);

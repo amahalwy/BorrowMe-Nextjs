534f6eadf702a372d7033217cee17932e054da5f
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-21 23:57:24 -0400

    Moved common types to redux/types file

--- redux/types.ts
+++ redux/types.ts
@@ -1,3 +1,4 @@
+// Generals
 export interface NewUser {
   firstName: string;
   lastName: string;
@@ -8,4 +9,49 @@ export interface NewUser {
   email: string;
   password: string;
   confirmPassword: string;
+}
+export interface Posting {
+  _id: string;
+  address: string;
+  city: string;
+  createdAt: string;
+  description: string;
+  image: string;
+  ownerId: string;
+  price: string;
+  state: string;
+  tags: string[];
+  title: string;
+  updatedAt: string;
+  zipCode: string;
+}
+export interface ModalPosting {
+  _id: string;
+  address: string;
+  city: string;
+  createdAt: string;
+  description: string;
+  image: string;
+  ownerId: string;
+  price: string;
+  state: string;
+  tags: string[];
+  title: string;
+  updatedAt: string;
+  zipCode: string;
+}
+export interface CurrentUser {
+  address: string;
+  city: string;
+  email: string;
+  exp: number;
+  firstName: string;
+  iat: number;
+  lastName: string;
+  postings: [];
+  profilePhoto: string;
+  state: string;
+  zipCode: string;
+  id?: string;
+  _id?: string;
 }

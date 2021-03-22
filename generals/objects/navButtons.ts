b0fed15337171d2d8d630f0fe3db3338679d8208
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-22 18:35:31 -0400

    Add navButtons objects to make creating menu items easier

---
+++ generals/objects/navButtons.ts
@@ -0,0 +1,41 @@
+export const __LOGGED_OUT_BUTTONS = [
+  {
+    path: "/landing",
+    text: "Landing",
+  },
+  {
+    path: "/signup",
+    text: "Signup",
+  },
+  {
+    path: "/login",
+    text: "Login",
+  },
+];
+
+export const __LOGGED_IN_BUTTONS = [
+  {
+    path: "/landing",
+    text: "Landing",
+  },
+  {
+    path: "/profile",
+    text: "Profile",
+  },
+  {
+    path: "/postings",
+    text: "Your postings",
+  },
+  {
+    path: "/requests",
+    text: "Outstanding requests",
+  },
+  {
+    path: "/bookings",
+    text: "Upcoming bookings",
+  },
+  {
+    path: "/home",
+    text: "Home",
+  },
+];

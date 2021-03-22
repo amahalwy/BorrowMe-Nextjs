12aa6b7d85767e5e51c2e5db403849cadf0f53fb
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-22 02:15:45 -0400

    Added another button variant for edit profile usage

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -30,6 +30,20 @@ const Button = {
         },
       },
     },
+    "edit-profile": {
+      bg: "brand.300",
+      color: "white",
+      w: "100%",
+      mb: "10px",
+      borderRadius: "25px",
+      fontSize: "1.3em",
+      _hover: {
+        bg: "brand.301",
+        _disabled: {
+          bg: "brand.302",
+        },
+      },
+    },
   },
 };
 

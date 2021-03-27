8c397595e10ccb240a7fad4943d22f73116e3882
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-27 00:30:37 -0400

    Fix to remove user from slice of state after logout

--- redux/reducers/sessionReducer.ts
+++ redux/reducers/sessionReducer.ts
@@ -14,16 +14,12 @@ const session = (state = initialState, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return {
-        ...state,
         isAuthenticated: !!action.currentUser,
         user: action.currentUser,
       };
     case RECEIVE_USER_LOGOUT:
-      state = undefined;
-      window.localStorage.removeItem("persist:root");
       return {
         isAuthenticated: false,
-        user: undefined,
       };
     case RECEIVE_USER_LOGIN:
       return Object.assign({}, state, { isSignedIn: true });

105329f840fcb78da8c1e08a5ba026bb3ce83eea
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-31 13:32:10 -0400

    Changed the way postings work to reflect search bar functionality

--- components/PostingsIndex.tsx
+++ components/PostingsIndex.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { ReduxState } from "../redux/types";
 import { fetchOne } from "../redux/util/postingsApiUtil";
@@ -9,19 +9,22 @@ import PostingItem from "./PostingItem";
 import PostingModal from "./PostingModal";
 import NotFound from "./Postings/NotFound";
 import LoadingPosting from "./Loading/LoadingPosting";
+import { RECEIVE_POSTINGS } from "../redux/actions/postingActions";
 
 const PostingsIndex: React.FC<PostingsIndexProps> = ({
   filteredList,
   setFilteredList,
 }) => {
   const modalPosting = useSelector((state: ReduxState) => state.entities.modal);
+  const dispatch = useDispatch();
   const [fetchOffset, setFetchOffset] = React.useState<number>(3);
 
   const fetchData = () => {
     fetchOne(fetchOffset).then((res) => {
       setFetchOffset(fetchOffset + 1);
-      const newPostings = filteredList.concat(res.data);
-      setFilteredList(newPostings);
+      const postings = filteredList.concat(res.data);
+      dispatch({ type: RECEIVE_POSTINGS, postings });
+      setFilteredList(postings);
     });
   };
 

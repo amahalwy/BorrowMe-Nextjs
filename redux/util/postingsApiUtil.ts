5e75017d973a3587e030dd35f8885507513d8f63
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-01 17:01:20 -0400

    Created new endpoint for searching a posting

--- redux/util/postingsApiUtil.ts
+++ redux/util/postingsApiUtil.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Posting } from "../types";
 
 export const fetchPostings = () => {
   return axios.get(
@@ -16,7 +17,7 @@ export const fetchInitial = () => {
   );
 };
 
-export const fetchOne = (num) => {
+export const fetchOne = (num: number) => {
   return axios.get(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings/fetchOne/${num}`
@@ -24,7 +25,7 @@ export const fetchOne = (num) => {
   );
 };
 
-export const fetchThree = (num) => {
+export const fetchThree = (num: number) => {
   return axios.get(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings/fetchThree/${num}`
@@ -32,7 +33,7 @@ export const fetchThree = (num) => {
   );
 };
 
-export const fetchPosting = (postingId) => {
+export const fetchPosting = (postingId: string) => {
   return axios.get(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings/${postingId}`
@@ -40,7 +41,7 @@ export const fetchPosting = (postingId) => {
   );
 };
 
-export const fetchUserPostings = (ownerId) => {
+export const fetchUserPostings = (ownerId: string) => {
   return axios.get(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/users/${ownerId}/postings`
@@ -48,7 +49,7 @@ export const fetchUserPostings = (ownerId) => {
   );
 };
 
-export const createPosting = (posting) => {
+export const createPosting = (posting: Posting) => {
   return axios.post(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings`
@@ -57,11 +58,19 @@ export const createPosting = (posting) => {
   );
 };
 
-export const updatePosting = (postingId, data) => {
+export const updatePosting = (postingId: string, data: any) => {
   return axios.patch(
     process.env.NODE_ENV === "production"
       ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings/${postingId}`
       : `${process.env.NEXT_PUBLIC_LOCAL_HOST_SERVER}/postings/${postingId}`,
     data
+  );
+};
+
+export const searchPosting = (query: string) => {
+  return axios.get(
+    process.env.NODE_ENV === "production"
+      ? `${process.env.NEXT_PUBLIC_DEPLOYED_HOST_SERVER}/postings/search/?${query}`
+      : `${process.env.NEXT_PUBLIC_LOCAL_HOST_SERVER}/postings/search/?${query}`
   );
 };

ff8cce3c57a941ab388ff49f2f402e7944ffe8fc
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-03 17:19:18 -0400

    Created loading animation for image load

--- components/PostingItem.tsx
+++ components/PostingItem.tsx
@@ -1,14 +1,23 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Image, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import LazyLoadImage from "./LazyLoadImage";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { clickPosting } from "../redux/actions/postingActions";
 import { PostingItemProps } from "../typescript/components";
+import { Img } from "react-image";
+import LoadingComponent from "./Loading/LoadingComponent";
 
-const PostingItem: React.FC<PostingItemProps> = ({ posting, clickPosting }) => {
+const PostingItem: React.FC<PostingItemProps> = ({ posting }) => {
+  const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    console.log("yepp");
+  }, []);
+
   return (
     <Box
-      m="4% 0"
+      mb="4%"
       p={3}
       shadow="md"
       borderWidth="1px"
@@ -16,29 +25,32 @@ const PostingItem: React.FC<PostingItemProps> = ({ posting, clickPosting }) => {
       borderRadius="md"
       bg="white"
     >
-      <Box mb="2%">
+      <Box>
         <Box mb="2%">
-          <Heading>{posting.title}</Heading>
+          <Box mb="2%">
+            <Heading>{posting.title}</Heading>
+          </Box>
+
+          <Box>
+            <Text>
+              Owner's note: <i>{posting.description}</i>
+            </Text>
+            <Text>Price: ${posting.price}/day</Text>
+          </Box>
         </Box>
 
-        <Box>
-          <Text>
-            Owner's note: <i>{posting.description}</i>
-          </Text>
-          <Text>Price: ${posting.price}/day</Text>
+        <Box maxH="400px" overflow="scroll" m="10px 0">
+          <Img src={posting.image} loader={<LoadingComponent />} />
         </Box>
+        <Button
+          onClick={() => dispatch(clickPosting(posting._id))}
+          variant="non-relative"
+        >
+          Open Calendar
+        </Button>
       </Box>
-
-      <Box maxH="400px" overflow="scroll" m="10px 0">
-        <LazyLoadImage src={posting.image} />
-      </Box>
-      <Button onClick={() => clickPosting(posting._id)}>Open Calendar</Button>
     </Box>
   );
 };
 
-const mDTP = {
-  clickPosting: clickPosting,
-};
-
-export default connect(null, mDTP)(PostingItem);
+export default PostingItem;
